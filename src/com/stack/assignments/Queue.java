0879ed6480cae7e9746e22842cc7a9e40a08660f
sambit420 <sambit2024@gmail.com>
2022-11-03 20:12:34 +0530

    Added : Use Case 4 Program of Queue - Dequeue the Queue from the begi

--- src/com/stack/assignments/Queue.java
+++ src/com/stack/assignments/Queue.java
@@ -1,33 +1,47 @@
 //Use Case 3 is to create a Queue of 56-->30-->70.
+//Use Case 4 is to dequeue from the beginning.
+
 package com.stack.assignments;
 
 import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Queue {
-
     //Declaring Linked List
     private static LinkedList list = new LinkedList();
 
     //Creating Queue method
-    public void enqueue(Object data) {
+    public void enqueue (Object data)
+    {
         list.add(data);
     }
 
     //Printing the top value
-    public Object peak() {
+    public Object peak()
+    {
         return list.getFirst();
     }
 
+    //Dequeue the top value
+    public Object poll()
+    {
+        return list.removeFirst();
+    }
+
     //Printing the values of Queue
-    public void printQueue() {
+    public void printQueue()
+    {
         System.out.println("Queue values are printed below");
-        if (!list.isEmpty()) {
+        if (!list.isEmpty())
+        {
             Iterator it = list.iterator();
-            while (it.hasNext()) {
-                System.out.print(it.next() + " ");
+            while (it.hasNext())
+            {
+                System.out.print(it.next()+" ");
             }
-        } else {
+        }
+        else
+        {
             System.out.println("Queue is empty");
         }
         System.out.println();
@@ -41,11 +55,19 @@ public class Queue {
         queue.enqueue(30);
         queue.enqueue(70);
 
-        //Printing the top element of stack
-        System.out.println("Top element of Queue is : " + queue.peak());
-
         //Printing the total stack
+        System.out.println("Printing the original Queue");
         queue.printQueue();
+
+        //Printing top element and dequeue it from Queue unless it is empty
+        while (!list.isEmpty())
+        {
+            System.out.println("Top element present in Queue is : "+queue.peak());
+            System.out.println("Dequeue top element from Queue : "+queue.poll());
+            System.out.println("Printing the Queue");
+            queue.printQueue();
+            System.out.println();
+        }
 
     }
 }
